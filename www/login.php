51edc9951c76a1725d8e234ee9d99f173cede651
Uchi (Manel) <zekrom.uchi@gmail.com>
2023-11-28 12:55:55 +0100

    f

--- www/login.php
+++ www/login.php
@@ -1,24 +1,24 @@
 <?php session_start();
-    $usuario='';
-    $pass='';
-    extract($_POST);
-    //var_dump($_POST);
-    if($usuario == '' || $pass == ''){
-        $mensa='Debe completar los campos';
-    }else{
-        require_once 'controladores/C_Usuarios.php';
-        $objUsuarios = new C_Usuarios();
-        $datos['usuario']=$usuario;
-        $datos['pass']=$pass;
-        // $resultado=$objUsuarios->validarUsuario($datos);
+$usuario = '';
+$pass = '';
+extract($_POST);
+//var_dump($_POST);
+if ($usuario == '' || $pass == '') {
+    $mensa = 'Debe completar los campos';
+} else {
+    require_once 'controladores/C_Usuarios.php';
+    $objUsuarios = new C_Usuarios();
+    $datos['usuario'] = $usuario;
+    $datos['pass'] = $pass;
+    // $resultado=$objUsuarios->validarUsuario($datos);
 
-        $resultado=$objUsuarios->validarUsuario(array('usuario'=>$usuario, 'pass'=>$pass));
-        if($resultado=='S'){
-            header('Location: index.php');
-        }else{
-            $mensa='Datos incorrectos';
-        }
+    $resultado = $objUsuarios->validarUsuario(array('usuario' => $usuario, 'pass' => $pass));
+    if ($resultado == 'S') {
+        header('Location: index.php');
+    } else {
+        $mensa = 'Datos incorrectos';
     }
+}
 ?>
 
 <!DOCTYPE html>
@@ -28,28 +28,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login2</title>
-    <link rel="stylesheet" href="./webfontkit-league-of-legends-fonts/stylesheet.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/reset.css">
-    
-    <link rel="stylesheet" href="./css/navbar.css">
-    <link rel="stylesheet" href="./css/login.css">
 </head>
 
 <body>
-    <form id="formularioLogin" method="post" action="login.php">
+    <div>
+        <form id="formularioLogin" method="post" action="login.php">
+            <div class="form-row">
+                <!-- Username input -->
+                <div class="form-group col-md-3">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon1"><i class="far fa-id-card"></i></span>
+                        </div>
+                        <input type="text" class="form-control" id="inputID" placeholder="Enter ID" aria-label="ID" aria-describedby="basic-addon1">
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <!-- Password input -->
+                <div class="form-group col-md-3">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="basic-addon2"><i class="fas fa-lock"></i></span>
+                        </div>
+                        <input type="password" class="form-control" id="inputPassword" placeholder="Password" aria-label="Password" aria-describedby="basic-addon2">
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-row">
+                <!-- Login button -->
+                <div class="form-group col-md-3">
+                    <button type="button" class="btn btn-primary">Login</button>
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <!-- <form id="formularioLogin" method="post" action="login.php">
         <div>
             <h1>LOGIN</h1>
             <div>
                 <div id="username-field" class="input-field">
                     <label for="usuario">USERNAME</label>
-                    <input type="text" id="usuario" name="usuario">  <br>
+                    <input type="text" id="usuario" name="usuario"> <br>
                 </div>
                 <div id="password-field" class="input-field">
                     <label for="pass">PASSWORD</label>
                     <input type="password" id="pass" name="pass">
                 </div>
                 <div>
-                    <?php echo $mensa ?>
+                    <?php /* echo $mensa */ ?>
                 </div>
             </div>
             <div>
@@ -58,5 +90,12 @@
             <br>
             <span>Ir Al <a href="/">Home</a></span>
         </div>
-    </form>
+    </form> -->
+
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/ed6377cf3c.js" crossorigin="anonymous"></script>
+
 </body>
